46936b4a77970f2d5adc2c53ddad523dfa60f677
Mr. Rizki <rizkioktav70@gmail.com>
2024-06-05 18:09:30 +0700

    [ADD and Solve Probs] : Feature in profile user/company and navbar

--- frontendAngular/src/app/components/profile/profile.component.ts
+++ frontendAngular/src/app/components/profile/profile.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  showCompanyList: boolean = true;
+  showCompanyProfile: boolean = false;
+  showAddCompanyForm: boolean = false;
+  showEditProfileUser: boolean = false;
+  showProfileUser: boolean = true;
+  showEditCompanyProfile: boolean = false;
+
+  ngOnInit(): void {
+    
+  }
+  toggleAddCompanyForm() {
+    this.showAddCompanyForm = !this.showAddCompanyForm;
+  }
+
+  cancelAddCompany() {
+    this.showAddCompanyForm = false;
+  }
+
+  addCompany() {
+    this.showAddCompanyForm = false;
+  }
+
+  showCompanyProfileCard() {
+    this.showCompanyList = false;
+    this.showCompanyProfile = true;
+    this.showEditCompanyProfile = false;
+  }
+
+  showCompanyListCard() {
+    this.showCompanyList = true;
+    this.showCompanyProfile = false;
+    this.showEditCompanyProfile = false;
+  }
+
+  showEditCompanyProfileCard(){
+    this.showEditCompanyProfile = true;
+    this.showCompanyList = false;
+    this.showCompanyProfile = false;
+  }
+  
+  showEditProfileUserCard() {
+    this.showEditProfileUser = true;
+    this.showProfileUser = false;
+  }
+
+  showProfileUserCard() {
+    this.showEditProfileUser = false;
+    this.showProfileUser = true;
+  }
 
 }
