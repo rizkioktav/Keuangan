5936f3ec8c0448366b57bbd2dc1a382d70fb9528
Mr. Rizki <rizkioktav70@gmail.com>
2024-06-18 11:00:48 +0700

    [EDIT]: Edit Environment

--- frontendAngular/src/environments/environment.prod.ts
+++ frontendAngular/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://bisniskitakeuangan.bikei.id/backendLaravel/public/api'
+  apiUrl: 'https://bisniskitakeuangan.bikei.id/backendLaravel/api'
 };
